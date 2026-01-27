f46a3aafadcbd3563eed7987df6d168ed441ff04
Sharon Park <sharon.park@autodesk.com>
2026-01-27 15:22:56 -0500

    Update umbrella header

--- Sources/OpenTracing/include/OpenTracing.h
+++ Sources/OpenTracing/include/OpenTracing.h
@@ -1,12 +1,25 @@
 //
 //  OpenTracing.h
-//  opentracing-objc
+//  OpenTracing
 //
 
-#import "OTGlobal.h"
-#import "OTNoop.h"
-#import "OTReference.h"
-#import "OTSpan.h"
-#import "OTSpanContext.h"
-#import "OTTracer.h"
-#import "OTVersion.h"
+#ifndef OpenTracing_h
+#define OpenTracing_h
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for OpenTracing.
+FOUNDATION_EXPORT double OpenTracingVersionNumber;
+
+//! Project version string for OpenTracing.
+FOUNDATION_EXPORT const unsigned char OpenTracingVersionString[];
+
+#import <OpenTracing/OTGlobal.h>
+#import <OpenTracing/OTNoop.h>
+#import <OpenTracing/OTReference.h>
+#import <OpenTracing/OTSpan.h>
+#import <OpenTracing/OTSpanContext.h>
+#import <OpenTracing/OTTracer.h>
+#import <OpenTracing/OTVersion.h>
+
+#endif /* OpenTracing_h */
