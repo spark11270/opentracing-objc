094c1ca31d2c77e77e16782af7acfc1c6ebbbd02
Sharon Park <sharon.park@autodesk.com>
2026-01-27 14:00:59 -0500

    Update opentracing.h

--- Sources/opentracing/include/opentracing.h
+++ Sources/opentracing/include/opentracing.h
@@ -3,23 +3,10 @@
 //  opentracing-objc
 //
 
-#ifndef opentracing_h
-#define opentracing_h
-
-#import <Foundation/Foundation.h>
-
-//! Project version number for opentracing.
-FOUNDATION_EXPORT double opentracingVersionNumber;
-
-//! Project version string for opentracing.
-FOUNDATION_EXPORT const unsigned char opentracingVersionString[];
-
-#import <opentracing/OTGlobal.h>
-#import <opentracing/OTNoop.h>
-#import <opentracing/OTReference.h>
-#import <opentracing/OTSpan.h>
-#import <opentracing/OTSpanContext.h>
-#import <opentracing/OTTracer.h>
-#import <opentracing/OTVersion.h>
-
-#endif /* opentracing_h */
+#import "OTGlobal.h"
+#import "OTNoop.h"
+#import "OTReference.h"
+#import "OTSpan.h"
+#import "OTSpanContext.h"
+#import "OTTracer.h"
+#import "OTVersion.h"
